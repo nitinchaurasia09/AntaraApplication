9ca799125a94522d2553ef3fbd46430fe3d1f3e9
Nitin Mukesh <nitin.mukesh@livehealthier.com>
2015-07-11 10:40:24 +0530

    Webapi

--- AntaraWebApi/Controllers/PageController.cs
+++ AntaraWebApi/Controllers/PageController.cs
@@ -5,12 +5,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
-using System.Web.Http.Cors;
+//using System.Web.Http.Cors;
 using System.Web.Mvc;
 
 namespace AntaraWebApi.Controllers
 {
-    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    //[EnableCors(origins: "*", headers: "*", methods: "*")]
     public class PageController : ApiController
     {
         PageRepository pageRepository;
