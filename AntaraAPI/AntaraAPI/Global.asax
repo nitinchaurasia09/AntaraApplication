b1868a2fe268cb05b08710b3765dca11185a611a
Nitin Mukesh <nitin.mukesh@livehealthier.com>
2015-07-11 15:32:07 +0530

    removed unused code

--- AntaraAPI/AntaraAPI/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AntaraAPI.WebApiApplication" Language="C#" %>
--- AntaraAPI/AntaraAPI/Repository/AntaraEntities.Designer.cs
+++
@@ -1,4 +0,0 @@
-﻿// Default code generation is disabled for model 'D:\Old project\My Project\AntaraApplication\AntaraAPI\AntaraAPI\Repository\AntaraEntities.edmx'. 
-// To enable default code generation, change the value of the 'Code Generation Strategy' designer
-// property to an alternate value. This property is available in the Properties Window when the model is
-// open in the designer.
